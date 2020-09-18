ee85592769aec6cdf6c417cc5811e9a1dd7bec8d
LostButStillImproving <mike6353@edu.easj.dk>
2020-09-18 11:17:16 +0200

    Added call method for RentersRente's main

--- src/MiniComputer.java
+++ src/MiniComputer.java
@@ -12,7 +12,6 @@ public class MiniComputer {
     public static void main(String[] args) {
 
         System.out.println("Velkommen til minicomputer!");
-
         Scanner input = new Scanner(System.in);
 
         for (int i = 0; i < 3; i++) {
@@ -21,7 +20,10 @@ public class MiniComputer {
 
             int x = input.nextInt();
 
-            if ( x == 1) { Terning.kast(); }
+            if ( x == 1) {
+                RentersRente.main(null);
+
+            }
             // Dine if sætninger skal komme her
             // Bil.dyt();
 
