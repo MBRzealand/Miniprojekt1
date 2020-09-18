68d1d2402bb5b5a7a492962f0559d2646d55c1ad
Mikkel B. Rasmussen <mikk24f6@edu.easj.dk>
2020-09-18 11:25:26 +0200

    program list added:

--- src/MiniComputer.java
+++ src/MiniComputer.java
@@ -11,7 +11,10 @@ public class MiniComputer {
 
     public static void main(String[] args) {
 
-        System.out.println("Velkommen til minicomputer!");
+        System.out.println("Velkommen til minicomputer!\nPick a program with the following numbers:");
+        System.out.println("\n\n");
+        System.out.println("\n1. Terninge Kast \n2. Renters Rente\n3. ASCII Art \n4. Convert \n5. Rock Paper Scissors \n6. Close Computer");
+
         Scanner input = new Scanner(System.in);
 
         for (int i = 0; i < 3; i++) {
