8421329804548497e941c257f3fd53b53b999f74
KRL1991 <kimx4706@edu.easj.dk>
2020-09-18 11:32:01 +0200

    Added Java Class ASCII Art and added three arts

--- src/ASCII_Art.java
+++ src/ASCII_Art.java
@@ -6,13 +6,57 @@ public class ASCII_Art {
 
     public static void main(String[] args) {
 
-        Scanner input = new Scanner(System.in);
-
-        //Promt the user to enter
+        ASCII_Drawings();
 
     }
 
-    public ASCII_Art(){
+    public static void ASCII_Drawings(){
+
+        //Promt the user to enter a number
+        System.out.println(" Indtast et nummer mellem 1-3 forskellig ASCII Art ");
+
+        Scanner input = new Scanner(System.in);
+        int x = input.nextInt();
+
+        if (x == 1) System.out.println("  :                \n" +
+                "               ;                \n" +
+                "              :                 \n" +
+                "              ;                 \n" +
+                "             /                  \n" +
+                "           o/                   \n" +
+                "         ._/\\___,                \n" +
+                "             \\                  \n" +
+                "             /                   \n" +
+                "             `   ");
+
+        if (x==2) System.out.println("   _----------_,\n" +
+                "    ,\"__         _-:, \n" +
+                "   /    \"\"--_--\"\"...:\\\n" +
+                "  /         |.........\\\n" +
+                " /          |..........\\\n" +
+                "/,         _'_........./:\n" +
+                "! -,    _-\"   \"-_... ,;;:\n" +
+                "\\   -_-\"         \"-_/;;;;\n" +
+                " \\   \\             /;;;;'\n" +
+                "  \\   \\           /;;;;\n" +
+                "   '.  \\         /;;;'\n" +
+                "     \"-_\\_______/;;'");
+
+        if (x==3) System.out.println("L L               /   \\               L L\n" +
+                "        __/|/|_             /  .  \\             _|\\|\\__\n" +
+                "       /_| [_[_\\           /     .-\\           /_]_] |_\\\n" +
+                "      /__\\  __`-\\_____    /    .    \\    _____/-`__  /__\\\n" +
+                "     /___] /=@>  _   {>  /-.         \\  <}   _  <@=\\ [___\\\n" +
+                "    /____/     /` `--/  /      .      \\  \\--` `\\     \\____\\\n" +
+                "   /____/  \\____/`-._> /               \\ <_.-`\\____/  \\____\\\n" +
+                "  /____/    /__/      /-._     .   _.-  \\      \\__\\    \\____\\\n" +
+                " /____/    /__/      /         .         \\      \\__\\    \\____\\\n" +
+                "|____/_  _/__/      /          .          \\      \\__\\_  _\\____|\n" +
+                " \\__/_ ``_|_/      /      -._  .        _.-\\      \\_|_`` _\\___/\n" +
+                "   /__`-`__\\      <_         `-;        NDT_>      /__`-`__\\\n" +
+                "      `-`           `-._       ;       _.-`           `-`\n" +
+                "                        `-._   ;   _.-`\n" +
+                "                            `-._.-` ");
 
 
     }
