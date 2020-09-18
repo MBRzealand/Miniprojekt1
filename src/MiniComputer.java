6148412d29bf4603bcc833056850ec991c102cf0
Mikkel B. Rasmussen <mikk24f6@edu.easj.dk>
2020-09-18 11:30:34 +0200

    program list formatted:

--- src/MiniComputer.java
+++ src/MiniComputer.java
@@ -11,15 +11,14 @@ public class MiniComputer {
 
     public static void main(String[] args) {
 
-        System.out.println("Velkommen til minicomputer!\nPick a program with the following numbers:");
-        System.out.println("\n\n");
-        System.out.println("\n1. Terninge Kast \n2. Renters Rente\n3. ASCII Art \n4. Convert \n5. Rock Paper Scissors \n6. Close Computer");
+        System.out.println("\nVelkommen til minicomputer!\n\nVælg et program med tallene:");
+        System.out.println("\n1. Terninge Kast \n2. Renters Rente\n3. ASCII Kunst \n4. Konverter \n5. Rock Paper Scissors \n6. Luk Computer");
 
         Scanner input = new Scanner(System.in);
 
         for (int i = 0; i < 3; i++) {
 
-            System.out.print("Hvilket miniprogram vil du køre [1..9]? ");
+            System.out.print("\nHvilket miniprogram vil du køre [1..9]? ");
 
             int x = input.nextInt();
 
