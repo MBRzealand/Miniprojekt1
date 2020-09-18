1894470c5dd4f7fef299ae896d9eaa170287886f
MMadsen99 <math9082@edu.easj.dk>
2020-09-18 12:00:36 +0200

    Sten saks papir

--- src/StenSaksPapir.java
+++ src/StenSaksPapir.java
@@ -3,15 +3,22 @@ import java.util.Random;
 
 
 public class StenSaksPapir {
-    Scanner input = new Scanner(System.in);
+
 
     public static void main(String[] args) {
-
-        System.out.println("Lad os spille sten saks papir");
         Scanner input = new Scanner(System.in);
+        System.out.println("Lad os spille sten saks papir");
+        System.out.println("1 = sten  2 = saks 3 = papir");
 
-        String PlayerPlay = " ";
+        String playerPlay = " ";
         int x = input.nextInt();
+        if (x == 1){
+            playerPlay = "Sten";
+        }else if (x == 2){
+            playerPlay = "Saks";
+        }else if (x == 3){
+            playerPlay = "Papir";
+        }
 
         String computerPlay = " ";
         Random generator = new Random();
@@ -24,7 +31,12 @@ public class StenSaksPapir {
         }else if (computerInt == 3);
             computerPlay = "Papir";
 
+        System.out.println("Du vælger: " + playerPlay);
         System.out.println("Computeren vælger: " + computerPlay);
+
+        if (playerPlay.equals(computerPlay)){
+            System.out.println("Det blev uafgjort");
+        }
     }
 
 }
