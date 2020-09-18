c409f58152e811f69a2c90a0a0b4f4bcd21d415a
MMadsen99 <math9082@edu.easj.dk>
2020-09-18 11:33:20 +0200

    Sten saks papir

---
+++ src/StenSaksPapir.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+import java.util.Random;
+
+
+public class StenSaksPapir {
+    Scanner input = new Scanner(System.in);
+
+    public static void main(String[] args) {
+
+        Random generator = new Random();
+        System.out.println("Lad os spille sten saks papir");
+
+
+        int computerInt = generator.nextInt();
+
+        if(computerInt == 1);
+
+
+
+
+}
