4cc13ec8f51d1a9b715026210a4cf82693c89a47
KRL1991 <kimx4706@edu.easj.dk>
2020-09-18 11:06:10 +0200

    Added Java Class ASCII Art

---
+++ src/ASCII_Art.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+// Made by Kim
+
+public class ASCII_Art {
+
+    public static void main(String[] args) {
+
+        Scanner input = new Scanner(System.in);
+
+        //Promt the user to enter
+
+    }
+
+    public ASCII_Art(){
+
+
+    }
+
+}
